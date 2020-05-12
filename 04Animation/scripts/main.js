ca4f6b036597a5297dfe57f400a1a47b7cd685fd
sarahliina <38528025+sarahliina@users.noreply.github.com>
2020-05-12 15:35:37 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,7 +1,7 @@
 
 
 // // changing individual properties with code and using setInterval
-var rotationSpeed = 0.06;
+var rotationSpeed = 0.10;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
