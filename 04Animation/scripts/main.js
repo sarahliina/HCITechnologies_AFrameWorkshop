f4477a012610866d68fa7f50fc777bf3418356e6
sarahliina <38528025+sarahliina@users.noreply.github.com>
2020-05-12 13:57:03 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,11 +1,11 @@
 
 
 // // changing individual properties with code and using setInterval
-var rotationSpeed = 0.01 * (-1);
+var rotationSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-  myOtherBox.object3D.rotation.x += rotationSpeed;
+  myOtherBox.object3D.rotation.x -= rotationSpeed;
 console.log(myOtherBox.object3D.rotation.x);
 }
 
