ce473d072006867efc9590190524bff32c1e84cc
sarahliina <38528025+sarahliina@users.noreply.github.com>
2020-05-21 13:04:47 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,6 +5,7 @@ var rotationSpeed = 0.1;
 var rotationSpeedY = 0.001;
 var myOtherBox = document.getElementById('myOtherBox');
 var check = 0;
+var checkSize = 0;
 
 function spin(){
  	myOtherBox.object3D.rotation.x += rotationSpeed *(-1);
@@ -18,19 +19,36 @@ function spin(){
 }
 
 function positionX(){
-  if (check <= 500) {
+  if (check <= 250) {
     myOtherBox.object3D.position.y += 0.05;
   }
 
-  else if (check > 500){
+  else if (check > 250){
     myOtherBox.object3D.position.y = myOtherBox.object3D.position.y - 0.05;
   }
 
   check++;
-  if (check==1000) {
+  if (check==500) {
     check = 0;
   }
 }
 
+function size() {
+  if (checkSize <= 250) {
+    myBox.object3D.width += 0.05;
+    myBox.object3D.height += 0.05;
+  }
+  else{
+    myBox.object3D.width -= 0.05;
+    myBox.object3D.height -= 0.05;
+  }
+  checkSize++;
+  if (checkSize == 500) {
+    checkSize = 0;
+  }
+
+}
+
 setInterval(spin, 16); //equivalent to 60 fps
 setInterval(positionX, 16);
+setInterval(checkSize, 16);
