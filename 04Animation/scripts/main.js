39e68b0ba1084a1033dcabaa7606dce90519e5a0
sarahliina <38528025+sarahliina@users.noreply.github.com>
2020-05-21 11:17:12 +0200

    Fast

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,7 +1,7 @@
 
 
 // // changing individual properties with code and using setInterval
-var rotationSpeed = 0.009;
+var rotationSpeed = 0.01;
 var rotationSpeedY = 0.001;
 var myOtherBox = document.getElementById('myOtherBox');
 
